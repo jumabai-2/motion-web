3897529da095442c34bd1383f944d876bd412ad4
jumabai-2 <aiypov.jumabai.itiu.2@gmail.com>
2022-11-27 16:15:40 +0600

    motion web

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
